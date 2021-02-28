4d6fd266f00a5a72b3927c85394c97daef711675
ssenaasnmz <ssenaasnmz@gmail.com>
2021-02-28 17:15:00 +0300

    8.Ders Proje Entity Framework

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -13,11 +14,11 @@ namespace DataAccess.Concrete.InMemory
         public InMemoryProductDal()
         {
             _products = new List<Product> {
-            new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitInStock=5, UnitPrice=10},
-            new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitInStock=500, UnitPrice=15},
-            new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitInStock=1500, UnitPrice=17},
-            new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitInStock=150, UnitPrice=8},
-            new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitInStock=65, UnitPrice=6},
+            new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitsInStock=5, UnitPrice=10},
+            new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitsInStock=5, UnitPrice=15},
+            new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitsInStock=1, UnitPrice=17},
+            new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitsInStock=1, UnitPrice=8},
+            new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitsInStock=6, UnitPrice=6},
             };
         }
 
@@ -33,9 +34,19 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(productToDelete);
         }
 
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
         {
             return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetAllByCategory(int categoryId)
@@ -49,7 +60,7 @@ namespace DataAccess.Concrete.InMemory
 
             productToUpdate.ProductName = product.ProductName;
             productToUpdate.CategoryId = product.CategoryId;
-            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
             productToUpdate.UnitPrice = product.UnitPrice;
         }
     }
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -5,13 +5,15 @@ using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Product:IEntity
+    public class Product : IEntity
     {
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
         public decimal UnitPrice { get; set; }
-        public short UnitInStock { get; set; }
+
+        
 
     }
 }
