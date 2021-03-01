50e5e7d0ae7ac88990fde7d521f53bf09fb6b2a9
ssenaasnmz <ssenaasnmz@gmail.com>
2021-03-01 16:34:48 +0300

    9.Ders 2.Mola

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,3 +10,4 @@ namespace DataAccess.Abstract
     {
     }
 }
+//Code Refactoring kodun iyileştirilmesi
